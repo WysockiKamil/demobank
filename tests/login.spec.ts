f61939533e47fd54c2032421e329740cebce4827
WysockiKamil <kamil.wysocki@protonmail.com>
2023-11-19 22:16:25 +0100

    first AAA implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,12 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
   test('Login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('testerLO');
-    await page.getByTestId('password-input').fill('asdfghjk');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = 'asdfghjk';
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('Login with correct too short login', async ({ page }) => {
