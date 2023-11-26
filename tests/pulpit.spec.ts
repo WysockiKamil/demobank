33bbe6f5aa9eec4612d349ec06efc917066a3aa9
WysockiKamil <kamil.wysocki@protonmail.com>
2023-11-26 01:47:46 +0100

    added beforeEch to pulpit tests

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,24 +3,23 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
     const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = 'asdfghjk';
+
     await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
   });
 
   test('Quick payment with correct data', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
-    const userPassword = 'asdfghjk';
-
     const receiverId = '2';
     const transferAmount = '150';
     const transferTitle = 'pizza';
     const expectedTransferReceiver = 'Chuck Demobankowy';
 
     // Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
@@ -35,18 +34,11 @@ test.describe('Pulpit tests', () => {
   });
   test('Successful mobile top-up', async ({ page }) => {
     //Arrange
-    const userId = 'testerLO';
-    const userPassword = 'asdfghjk';
-
     const topUpReceiver = '500 xxx xxx';
     const topUpAmount = '150';
     const expectedMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`;
 
     //Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
     await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#uniform-widget_1_topup_agreement span').click();
